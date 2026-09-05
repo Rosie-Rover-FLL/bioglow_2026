507eea03205d327163b6e98c95befcb201c24aa9
David Fisher <fisherds@rose-hulman.edu>
2026-09-04 23:12:09 -0400

    Miriam's Drone is M1

--- m1.py
+++ m1.py
@@ -2,15 +2,9 @@ import rosie_rover
 from pybricks.tools import wait
 
 def run(robot):
-    robot.drive_base.settings(straight_speed=100)
-    robot.drive_base.straight(700)
-    wait(1000)
-    robot.drive_base.turn(45)
-    wait(500)
-    robot.drive_base.straight(-40)
-    robot.drive_base.turn(25)
-    wait(1000)
-    robot.drive_base.straight(180)
+    robot.drive_base.settings(straight_speed=800)
+    robot.drive_base.straight(-550)
+    robot.drive_base.straight(550)
 
 
 if __name__ == "__main__":
--- m3.py
+++ m3.py
@@ -3,29 +3,39 @@ from pybricks.tools import wait
 
 
 def run(robot):
+    robot.drive_base.settings(straight_speed=800)
+    robot.drive_base.straight(700)
+    wait(1000)
+    robot.drive_base.turn(45)
+    wait(500)
+    robot.drive_base.straight(-40)
+    robot.drive_base.turn(25)
+    wait(1000)
+    robot.drive_base.straight(180)
+
     # Fake demo mission: like m2, but the arm phases are split around the
     # return trip instead of both happening before it, reversed (down
     # first, then up), slower, and shorter.
 
-    robot.drive_base.drive(200, 0)
-    wait(2000)
-    robot.drive_base.stop()
+    # robot.drive_base.drive(200, 0)
+    # wait(2000)
+    # robot.drive_base.stop()
 
-    robot.left_top_motor.dc(-35)
-    robot.right_top_motor.dc(-35)
-    wait(500)
-    robot.left_top_motor.dc(0)
-    robot.right_top_motor.dc(0)
+    # robot.left_top_motor.dc(-35)
+    # robot.right_top_motor.dc(-35)
+    # wait(500)
+    # robot.left_top_motor.dc(0)
+    # robot.right_top_motor.dc(0)
 
-    robot.drive_base.drive(-200, 0)
-    wait(2000)
-    robot.drive_base.stop()
+    # robot.drive_base.drive(-200, 0)
+    # wait(2000)
+    # robot.drive_base.stop()
 
-    robot.left_top_motor.dc(35)
-    robot.right_top_motor.dc(35)
-    wait(500)
-    robot.left_top_motor.dc(0)
-    robot.right_top_motor.dc(0)
+    # robot.left_top_motor.dc(35)
+    # robot.right_top_motor.dc(35)
+    # wait(500)
+    # robot.left_top_motor.dc(0)
+    # robot.right_top_motor.dc(0)
 
 
 if __name__ == "__main__":
